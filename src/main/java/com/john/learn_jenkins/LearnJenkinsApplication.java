fecb6a0988b7b6447a544fd294ab4ee7cff2eb7e
johnsungjs <johnsungjs@gmail.com>
2024-11-21 12:06:48 +0700

    init commit

---
+++ src/main/java/com/john/learn_jenkins/LearnJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.john.learn_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/john/learn_jenkins/LearnJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.john.learn_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
