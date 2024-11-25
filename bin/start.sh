55c8cc3813b3b1aeb87c2c58f934b8092fe82452
sungjohnjs <sungjohnjs@gmail.com>
2024-11-25 09:33:18 +0700

    change to java jar command without and sign

--- bin/start.sh
+++ bin/start.sh
@@ -3,4 +3,6 @@
 # nohup java -jar ./target/learn_jenkins-0.0.1.jar >log/app.log 2>&1 &
 # echo \$! > pid.file
 
-mvn spring-boot:run
+# mvn spring-boot:run
+
+java -jar ./target/learn_jenkins-0.0.1.jar
