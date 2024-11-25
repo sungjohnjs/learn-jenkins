aa64610bba13bf47af62d2a85eef65c68b905e6d
sungjohnjs <sungjohnjs@gmail.com>
2024-11-25 09:28:43 +0700

    change start script to maven run

--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-nohup java -jar ./target/learn_jenkins-0.0.1.jar >log/app.log 2>&1 &
-echo \$! > pid.file
+# nohup java -jar ./target/learn_jenkins-0.0.1.jar >log/app.log 2>&1 &
+# echo \$! > pid.file
+
+mvn spring-boot:run &
