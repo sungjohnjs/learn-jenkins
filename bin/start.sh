3237391f6d090f5489f73afe156adf39c728f979
sungjohnjs <sungjohnjs@gmail.com>
2024-11-25 09:37:22 +0700

    add disown

--- bin/start.sh
+++ bin/start.sh
@@ -5,4 +5,8 @@
 
 # mvn spring-boot:run
 
-java -jar ./target/learn_jenkins-0.0.1.jar
+# java -jar ./target/learn_jenkins-0.0.1.jar
+
+java -jar ./target/learn_jenkins-0.0.1.jar > log/app.log 2>&1 &
+disown
+
