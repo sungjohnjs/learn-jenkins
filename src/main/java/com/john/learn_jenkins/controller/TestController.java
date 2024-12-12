06610e38795e5af846d4d78e8edf482b83b73cda
sungjohnjs <sungjohnjs@gmail.com>
2024-12-12 09:34:17 +0700

    add log slf4j

--- src/main/java/com/john/learn_jenkins/controller/TestController.java
+++ src/main/java/com/john/learn_jenkins/controller/TestController.java
@@ -4,11 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
-@RequestMapping(value="")
+@RequestMapping(value = "")
+@Slf4j
 public class TestController {
-  @GetMapping("")
+
+    @GetMapping("")
     public String getTest() {
+        log.info("MASUK TEST CONTROLLER");
 
         return "Halo Jenkins 06";
     }
