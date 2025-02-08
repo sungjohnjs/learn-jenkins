398d1791652fd739c71307ec69b064a73a78ef69
johnsungjs <johnsungjs@gmail.com>
2025-02-08 15:54:06 +0700

    remove log setting

--- src/main/java/com/john/learn_jenkins/controller/TestController.java
+++ src/main/java/com/john/learn_jenkins/controller/TestController.java
@@ -4,17 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.slf4j.Slf4j;
-
 @RestController
 @RequestMapping(value = "")
-@Slf4j
 public class TestController {
 
     @GetMapping("")
     public String getTest() {
-        log.info("MASUK TEST CONTROLLER");
-
         return "Halo Jenkins 06";
     }
 }
